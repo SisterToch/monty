06d68e466f47194782c842d795b5410b90ccbf09
SisterToch <ibekwetestimony3@gmail.com>
2023-08-23 15:27:12 -0700

    add

--- add.c
+++ add.c
@@ -1,19 +1,19 @@
 #include "monty.h"
-
 /**
- * add - Adds the top two elements of the stack.
- * @stack: A pointer to the stack.
- * @line_number: The line number of the instruction.
+ * add -this funtion adds the top two elements
+ * present in the stack
+ * @stack: points to the head of the stack.
+ * @number: acts like a counter, telling the number
+ * of the instructions given.
  */
-void add(stack_t **stack, unsigned int line_number)
+void add(stack_t **stack, unsigned int number)
 {
-    if (*stack == NULL || (*stack)->next == NULL)
-    {
-        fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", number);
+		exit(EXIT_FAILURE);
+	}
 
-    (*stack)->next->n += (*stack)->n;
-    pop(stack, line_number); /*Remove the top element*/
+	(*stack)->next->n += (*stack)->n;
+	pop(stack, number);
 }
-
